efa52a564f40d8ea61e87fe20574ca67a8e185fe
deepalikonety <deepalikonety@gmail.com>
2025-07-16 00:20:14 +0530

    last update

--- lexiapi/settings.py
+++ lexiapi/settings.py
@@ -132,5 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://workspace-dashboard-lofz.vercel.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
